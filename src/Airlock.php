ee48e7b1fc2c8e7c3d2eab437583ec97b68fcbb9
Brian Faust <faustbrian@users.noreply.github.com>
2020-01-09 07:04:51 +0200

    use configured model first for userModel

--- src/Airlock.php
+++ src/Airlock.php
@@ -27,7 +27,7 @@ class Airlock
      */
     public static function userModel()
     {
-        return static::$userModel;
+        return config('auth.providers.users.model', static::$userModel);
     }
 
     /**
@@ -54,7 +54,7 @@ class Airlock
             return static::$runsMigrations;
         }
 
-        $model = config('auth.providers.users.model', static::userModel());
+        $model = static::userModel();
 
         return class_exists($model)
                     ? in_array(HasApiTokens::class, class_uses_recursive($model))
