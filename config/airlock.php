bd52765754cc9bbb10ebc100d09702619a3ba02e
Riadh KHEDHIRI <riadh.khedhiri@gmail.com>
2020-03-13 18:31:31 +0100

    Add an env variable

--- config/airlock.php
+++ config/airlock.php
@@ -43,6 +43,6 @@ return [
         'verify_csrf_token' => App\Http\Middleware\VerifyCsrfToken::class,
     ],
 
-    'name' => 'airlock.csrf',
+    'name' => env('AIRLOCK_ROUTE_NAME', 'airlock.csrf'),
 
 ];
