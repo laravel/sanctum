a1ca1891b90e8e17cb3fb3f5f05200c49157393c
Alexandr Smirnov <phpclub@gmail.com>
2020-06-10 11:39:15 +0300

    Update sanctum.php

--- config/sanctum.php
+++ config/sanctum.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost,127.0.0.1,::1',)),
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost,127.0.0.1,[::1]',)),
 
     /*
     |--------------------------------------------------------------------------
