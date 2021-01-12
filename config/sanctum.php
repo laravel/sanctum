e9ad83a9777e3969ab0f4d2165c97b782880d555
Grzegorz Rozycki <grozycki@op.pl>
2021-01-12 13:11:53 +0100

    Added APP_URL to list of stateful domains

--- config/sanctum.php
+++ config/sanctum.php
@@ -13,10 +13,22 @@ return [
     |
     */
 
-    'stateful' => explode(',', env(
-        'SANCTUM_STATEFUL_DOMAINS',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1'
-    )),
+    'stateful' => Illuminate\Support\Str::of(
+        env(
+            'SANCTUM_STATEFUL_DOMAINS',
+            env('APP_URL') . ',localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1'
+        )
+    )
+        ->explode(',')
+        ->map(
+            function ($host) {
+                return preg_replace('#^https?://#', '', $host);
+            }
+        )
+        ->filter()
+        ->unique()
+        ->values()
+        ->all(),
 
     /*
     |--------------------------------------------------------------------------
