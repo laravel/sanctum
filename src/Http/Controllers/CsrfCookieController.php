cc89f3046643763e32caf4c5d6467c877d409783
Taylor Otwell <taylor@laravel.com>
2021-01-24 19:14:18 -0600

    Update CsrfCookieController.php

--- src/Http/Controllers/CsrfCookieController.php
+++ src/Http/Controllers/CsrfCookieController.php
@@ -11,6 +11,7 @@ class CsrfCookieController
     /**
      * Return an empty response simply to trigger the storage of the CSRF cookie in the browser.
      *
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function show(Request $request)
