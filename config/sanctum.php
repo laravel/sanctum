9f8a27760b58ce5b7084051e374c26346c37a87e
Miha Penger <miha.penger@avantcar.si>
2021-11-04 12:23:34 +0100

    Make localhost:8000 stateful (fixes 401 Unauthorized error at SPA authentication)

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,7 +15,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        'localhost,localhost:8000,127.0.0.1,127.0.0.1:8000,::1',
         env('APP_URL') ? ','.parse_url(env('APP_URL'), PHP_URL_HOST) : ''
     ))),
 
