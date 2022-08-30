e1f573e9a71aa80d871b72589eb882f4a1076f50
Mohamed Gaber <mgaber@tendegrees.sa>
2022-08-30 13:26:34 +0200

    add migration file for adding expires_at field

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -20,7 +20,6 @@ return new class extends Migration
             $table->string('token', 64)->unique();
             $table->text('abilities')->nullable();
             $table->timestamp('last_used_at')->nullable();
-            $table->timestamp('expires_at')->nullable();
             $table->timestamps();
         });
     }
