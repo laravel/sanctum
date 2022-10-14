b3e782e2f686e5ec0a1b1975c0d2bf5b53e50d43
Vorname Nachname <vorname.nachname@mll.com>
2022-10-14 19:48:18 +0200

    add missing expiresAt parameter to contract

--- src/Contracts/HasApiTokens.php
+++ src/Contracts/HasApiTokens.php
@@ -2,6 +2,8 @@
 
 namespace Laravel\Sanctum\Contracts;
 
+use DateTimeInterface;
+
 interface HasApiTokens
 {
     /**
@@ -24,9 +26,10 @@ interface HasApiTokens
      *
      * @param  string  $name
      * @param  array  $abilities
+     * @param  \DateTimeInterface|null  $expiresAt
      * @return \Laravel\Sanctum\NewAccessToken
      */
-    public function createToken(string $name, array $abilities = ['*']);
+    public function createToken(string $name, array $abilities = ['*'], DateTimeInterface $expiresAt = null);
 
     /**
      * Get the access token currently associated with the user.
