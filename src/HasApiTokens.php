d132ec4a59953a3fac370cc43e31719667a53821
Alberto Elias <41304037+albertoelias123@users.noreply.github.com>
2023-06-15 12:53:40 -0300

    Fix return type using Intersection Types (PHP 8.1)
    The `currentAccessToken()` method was updated to fix the return type declaration. Intersection Types from PHP 8.1 were used to specify that the returned object is an instance of `Illuminate\Database\Eloquent\Model` and also implements the `Laravel\Sanctum\Contracts\HasAbilities` interface.
    
    By utilizing the `Model&HasAbilities` type in the PHPDoc, we accurately document that the method returns an object that satisfies both the `Model` class requirement and the `HasAbilities` interface implementation.
    
    Note: Please ensure that your Laravel project is updated to the latest version, as it requires PHP 8.1 or a later version to utilize Intersection Types.

--- src/HasApiTokens.php
+++ src/HasApiTokens.php
@@ -58,7 +58,7 @@ trait HasApiTokens
     /**
      * Get the access token currently associated with the user.
      *
-     * @return \Laravel\Sanctum\PersonalAccessToken|null
+     * @return \Laravel\Sanctum\Contracts\HasAbilities&\Illuminate\Database\Eloquent\Model
      */
     public function currentAccessToken()
     {
