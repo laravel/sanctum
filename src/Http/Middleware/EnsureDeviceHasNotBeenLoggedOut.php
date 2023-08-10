9392ec9b3d08540fbe8441b123a7690c54e1a554
Patrick O'Meara <pat@fixd.io>
2023-08-10 22:39:35 +1000

    Ensure device has not been logged out
    This adds a middleware to check that the password has in session is the same as the current users password.
    
    This fixes a security issue where an attacker can keep sending requests to an API using the sanctum auth after the password has been changed.

---
+++ src/Http/Middleware/EnsureDeviceHasNotBeenLoggedOut.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Laravel\Sanctum\Http\Middleware;
+
+use Closure;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Contracts\Auth\Factory as AuthFactory;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureDeviceHasNotBeenLoggedOut
+{
+    public function __construct(protected AuthFactory $auth)
+    {
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (! $request->hasSession() || ! $request->user()) {
+            return $next($request);
+        }
+
+        if ($request->session()->get('password_hash_'.$this->auth->getDefaultDriver()) !== $request->user()->getAuthPassword()) {
+            $this->logout($request);
+
+            throw new AuthenticationException('Unauthenticated.', [$this->auth->getDefaultDriver()]);
+        }
+
+        return $next($request);
+    }
+
+    protected function logout(Request $request)
+    {
+        $this->auth->logoutCurrentDevice();
+
+        $request->session()->flush();
+    }
+}
