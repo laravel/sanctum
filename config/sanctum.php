338f633e6487e76b255470d3373fbc29228aa971
Taylor Otwell <taylor@laravel.com>
2023-09-07 10:46:33 -0500

    wip

--- config/sanctum.php
+++ config/sanctum.php
@@ -75,7 +75,7 @@ return [
     */
 
     'middleware' => [
-        'authenticate_session' => \Laravel\Sanctum\Http\Middleware\AuthenticateSession::class,
+        'authenticate_session' => Laravel\Sanctum\Http\Middleware\AuthenticateSession::class,
         'encrypt_cookies' => App\Http\Middleware\EncryptCookies::class,
         'verify_csrf_token' => App\Http\Middleware\VerifyCsrfToken::class,
     ],
