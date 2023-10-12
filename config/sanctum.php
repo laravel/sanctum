dd3ee2ac7be88425f3cc4efb83f0c337ca76377e
Luke Kuzmish <42181698+cosmastech@users.noreply.github.com>
2023-10-12 07:58:52 -0400

    fix typo

--- config/sanctum.php
+++ config/sanctum.php
@@ -54,7 +54,7 @@ return [
     |--------------------------------------------------------------------------
     |
     | Sanctum can prefix new tokens in order to take advantage of various
-    | security scanning initiaives maintained by open source platforms
+    | security scanning initiatives maintained by open source platforms
     | that alert developers if they commit tokens into repositories.
     |
     | See: https://docs.github.com/en/code-security/secret-scanning/about-secret-scanning
