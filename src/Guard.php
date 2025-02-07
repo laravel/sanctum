21102986c4649aa9f37edbc48d4adc65b50795f3
Aminul Islam <5006546+AminulBD@users.noreply.github.com>
2025-02-07 21:21:06 +0600

    use correct guard for multi-guard

--- src/Guard.php
+++ src/Guard.php
@@ -54,7 +54,12 @@ class Guard
     public function __invoke(Request $request)
     {
         foreach (Arr::wrap(config('sanctum.guard', 'web')) as $guard) {
-            if ($user = $this->auth->guard($guard)->user()) {
+            $user = $this->auth->guard($guard)->user();
+            if (! $user) {
+                continue;
+            }
+
+            if (is_null($this->provider) || $this->hasValidProvider($user)) {
                 return $this->supportsTokens($user)
                     ? $user->withAccessToken(new TransientToken)
                     : $user;
