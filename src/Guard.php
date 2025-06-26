6e1612edd2f231b0d077cdb2ea917ae25b65bf34
Luke Kuzmish <42181698+cosmastech@users.noreply.github.com>
2025-06-26 12:46:22 -0400

    [4.x] Factor token `last_used_at` update into separate method (#567)
    * factor our time token was last used update
    
    * remove tap()
    
    * Update Guard.php
    
    ---------
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Guard.php
+++ src/Guard.php
@@ -77,16 +77,7 @@ class Guard
 
             event(new TokenAuthenticated($accessToken));
 
-            if (method_exists($accessToken->getConnection(), 'hasModifiedRecords') &&
-                method_exists($accessToken->getConnection(), 'setRecordModificationState')) {
-                tap($accessToken->getConnection()->hasModifiedRecords(), function ($hasModifiedRecords) use ($accessToken) {
-                    $accessToken->forceFill(['last_used_at' => now()])->save();
-
-                    $accessToken->getConnection()->setRecordModificationState($hasModifiedRecords);
-                });
-            } else {
-                $accessToken->forceFill(['last_used_at' => now()])->save();
-            }
+            $this->updateLastUsedAt($accessToken);
 
             return $tokenable;
         }
@@ -182,5 +173,24 @@ class Guard
         $model = config("auth.providers.{$this->provider}.model");
 
         return $tokenable instanceof $model;
+    }
+
+    /**
+     * Store the time the token was last used.
+     *
+     * @param  \Laravel\Sanctum\PersonalAccessToken  $accessToken
+     * @return void
+     */
+    protected function updateLastUsedAt($accessToken)
+    {
+        if (method_exists($accessToken->getConnection(), 'hasModifiedRecords') &&
+            method_exists($accessToken->getConnection(), 'setRecordModificationState')) {
+            $hasModifiedRecords = $accessToken->getConnection()->hasModifiedRecords();
+            $accessToken->forceFill(['last_used_at' => now()])->save();
+
+            $accessToken->getConnection()->setRecordModificationState($hasModifiedRecords);
+        } else {
+            $accessToken->forceFill(['last_used_at' => now()])->save();
+        }
     }
 }
