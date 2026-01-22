224864063d51d6adddc8c061ccf61eafa0e8f7ce
Jack Bayliss <jjbayliss@icloud.com>
2026-01-22 22:26:39 +0000

    [4.x] Use property promotion (#586)
    * property promotion bits
    
    * Update tests.yml
    
    * god damn full stops

--- src/Http/Middleware/AuthenticateSession.php
+++ src/Http/Middleware/AuthenticateSession.php
@@ -14,20 +14,12 @@ use Symfony\Component\HttpFoundation\Response;
 class AuthenticateSession
 {
     /**
-     * The authentication factory implementation.
-     *
-     * @var \Illuminate\Contracts\Auth\Factory
-     */
-    protected $auth;
-
-    /**
      * Create a new middleware instance.
      *
-     * @param  \Illuminate\Contracts\Auth\Factory  $auth
+     * @param  \Illuminate\Contracts\Auth\Factory  $auth The authentication factory implementation.
      */
-    public function __construct(AuthFactory $auth)
+    public function __construct(protected AuthFactory $auth)
     {
-        $this->auth = $auth;
     }
 
     /**
